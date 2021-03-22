758f5603f4b03c16b76a79a62c6c3c1cfe534e6c
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-03-21 21:17:14 -0300

    Update Area_Drag.java

--- Classes-Figuras/Area_Drag.java
+++ Classes-Figuras/Area_Drag.java
@@ -1,13 +1,12 @@
 public class RectApp {
     public static void main (String[] args) {
         Rect r1 = new Rect(1,1, 10,10);
-	r1.area();
 	r1.drag(20,30);
         r1.print();
+	System.out.format("Area = %d.\n",r1.area());
     }
 }
 class Rect {
-    int a;
     int x, y;
     int w, h;
     Rect (int x, int y, int w, int h) {
@@ -17,16 +16,16 @@ class Rect {
         this.h = h;
     }
     int area () {
-	this.a = (this.w) * (this.h);
-	return this.a;
+	int a = (this.w) * (this.h);
+	return a;
     }
     void drag (int dx, int dy) {
 	this.x = this.x + dx;
 	this.y = this.y + dy;
     }
     void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) e area (%d), movido para posicao (%d,%d).\n",
-            this.w, this.h, this.a, this.x, this.y);
+        System.out.format("Retangulo de tamanho (%d,%d), movido para posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
     }
     
 }
