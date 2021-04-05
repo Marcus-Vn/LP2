b755a2fd493e05d19ba78fb688268de6951fa670
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-05 18:49:48 -0300

    Figura Elipse

---
+++ 07 - Figures/figures/Ellipse.java
@@ -0,0 +1,28 @@
+package figures;
+
+import java.awt.*;
+
+public class Ellipse extends Figure{
+    int x, y;
+    int w, h;
+    //background,outline
+    Color bgd, ol;
+
+    public Ellipse (int x, int y, int w, int h, Color bgd, Color ol) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+	this.bgd = bgd;
+	this.ol = ol;
+    }
+    public void paint (Graphics g) {
+	Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.bgd);
+	g2d.fillOval(this.x,this.y,this.w,this.h);
+	g2d.setColor(this.ol); 
+	g2d.drawOval(this.x,this.y,this.w,this.h); 
+        
+    }
+    
+}
