4c5e4209a7c30dc2ebc9247fb9c4060568295ca6
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-18 16:55:27 -0300

    Retangulo atualizado

--- Projeto/figures/Rect.java
+++ Projeto/figures/Rect.java
@@ -1,11 +1,13 @@
 package figures;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.event.*;
 
 public class Rect extends Figure {
-
-    Color bgd;
-
+  	
+    Rectangle2D Rect;    
+    
     public Rect (int x, int y, int w, int h, Color ol, Color bgd) {
         this.x = x;
         this.y = y;
@@ -18,11 +20,34 @@ public class Rect extends Figure {
 
     public void paint (Graphics g) {
 	Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.bgd);
-	g2d.fillRect(this.x,this.y,this.w,this.h);
+	this.Rect = new Rectangle2D.Double(this.x,this.y,this.w,this.h);
+        g2d.setStroke(new BasicStroke(2));
+	g2d.setColor(this.bgd);
+	g2d.fill(this.Rect);
 	g2d.setColor(this.ol); 
-	g2d.drawRect(this.x,this.y,this.w,this.h); 
+	g2d.draw(this.Rect); 
         
+    }
+	
+
+    public boolean contains(MouseEvent evt) {
+        if (this.Rect.contains(evt.getPoint())){
+            	return true;
+	}
+	else{ return false;}
+    }
+
+    public void drag (int dx, int dy) {
+
+	this.x += dx;
+	this.y += dy;
+	this.Rect = new Rectangle2D.Double(this.x,this.y,this.w,this.h);
+    }
+
+    public void redimension(int d) {
+	this.w += d;
+	this.h += d;
+	this.Rect = new Rectangle2D.Double(this.x,this.y,this.w,this.h);
     }
     
 }
