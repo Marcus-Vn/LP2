fe39da8927005aea81b5a4ededb948daab57ccfb
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-18 16:56:07 -0300

    Seta Atualizada

--- Projeto/figures/Seta.java
+++ Projeto/figures/Seta.java
@@ -1,13 +1,14 @@
 package figures;
 
 import java.awt.*;
-
-import java.util.Random;
+import java.awt.event.*;
 
 public class Seta extends Figure {
+
+    Polygon Seta;	
+
     //extensão da ponta
     int l, p;
-    Color bgd;
 
     public Seta (int x, int y, int w, int h, int l, int p, Color ol, Color bgd) {
         this.x = x;
@@ -27,12 +28,49 @@ public class Seta extends Figure {
         int[] yValues = { this.y, this.y, this.y - this.l, this.y + this.h/2,
 	this.l+this.h+this.y, this.y + this.h, this.y + this.h};
 	
-	Polygon seta = new Polygon( xValues, yValues, 7 );
-        g2d.setColor(this.bgd);
-	g2d.fillPolygon( seta ); 
+	this.Seta = new Polygon( xValues, yValues, 7 );
+        g2d.setStroke(new BasicStroke(2));
+	g2d.setColor(this.bgd);
+	g2d.fill(this.Seta); 
 	g2d.setColor(this.ol); 
-	g2d.drawPolygon( seta ); 
+	g2d.draw(this.Seta); 
         
+    }
+	
+    public boolean contains(MouseEvent evt) {
+        if (this.Seta.contains(evt.getPoint())){
+            	return true;
+	}
+	else{ return false;}
+    }
+
+    public void drag (int dx, int dy) {
+	this.x += dx;
+	this.y += dy;
+	int[] xValues = { this.x, this.x+this.w, this.x+this.w, this.x+this.w+this.p, 
+	this.x + this.w, this.x + this.w, this.x};
+        int[] yValues = { this.y, this.y, this.y - this.l, this.y + this.h/2,
+	this.l+this.h+this.y, this.y + this.h, this.y + this.h};
+	this.Seta = new Polygon( xValues, yValues, 7 );
+    }
+
+    public void redimension(int d) {
+	this.w += d;
+	this.h += d;
+	if(d < 0){
+		if(this.p >= 6 && this.l >=6){
+			this.p += d;
+			this.l += d;
+		}
+	}else{
+		this.p += d;
+		this.l += d;
+	}
+	int[] xValues = { this.x, this.x+this.w, this.x+this.w, this.x+this.w+this.p, 
+	this.x + this.w, this.x + this.w, this.x};
+        int[] yValues = { this.y, this.y, this.y - this.l, this.y + this.h/2,
+	this.l+this.h+this.y, this.y + this.h, this.y + this.h};
+	this.Seta = new Polygon( xValues, yValues, 7 );
     }
     
 }
