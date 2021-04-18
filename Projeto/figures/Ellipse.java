7d14d3144f95f09d14ba30cc45d8af115b8056e6
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-18 16:53:49 -0300

    Ellipse Atualizada

--- Projeto/figures/Ellipse.java
+++ Projeto/figures/Ellipse.java
@@ -1,11 +1,13 @@
 package figures;
 
 import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
 
 public class Ellipse extends Figure{
 
-    Color bgd;
-	
+    Ellipse2D Ellipse;
+
     public Ellipse (int x, int y, int w, int h, Color ol, Color bgd) {
         this.x = x;
         this.y = y;
@@ -14,13 +16,34 @@ public class Ellipse extends Figure{
 	this.ol = ol;
 	this.bgd = bgd;
     }
+
     public void paint (Graphics g) {
 	Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(this.bgd);
-	g2d.fillOval(this.x,this.y,this.w,this.h);
+	this.Ellipse = new Ellipse2D.Double(this.x,this.y,this.w,this.h);
+        g2d.setStroke(new BasicStroke(2));
+	g2d.setColor(this.bgd);
+	g2d.fill(this.Ellipse);
 	g2d.setColor(this.ol); 
-	g2d.drawOval(this.x,this.y,this.w,this.h); 
+	g2d.draw(this.Ellipse); 
         
     }
     
+    public boolean contains(MouseEvent evt) {
+        if (this.Ellipse.contains(evt.getPoint())){
+            	return true;
+	}
+	else{ return false;}
+    }
+
+    public void drag (int dx, int dy) {
+	this.x += dx;
+	this.y += dy;
+	this.Ellipse = new Ellipse2D.Double(this.x,this.y,this.w,this.h);
+    }
+    
+    public void redimension (int d) {
+	this.w += d;
+	this.h += d;
+	this.Ellipse = new Ellipse2D.Double(this.x,this.y,this.w,this.h);
+    }
 }
