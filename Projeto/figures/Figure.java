7635d7183ebb6d7aee249cb4f15cd80217099eaa
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-18 16:54:31 -0300

    Figures Atualizada

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -1,12 +1,17 @@
 package figures;
 
 import java.awt.*;
+import java.awt.event.*;
 
 public abstract class Figure {
     public abstract void paint (Graphics g);
     public int x, y;
     public int w, h;
     //background,outline
-    public Color ol;
+    public Color bgd, ol;
+
+    public abstract boolean contains(MouseEvent evt);
+    public abstract void drag(int dx, int dy);
+    public abstract void redimension(int d);
     
 }
