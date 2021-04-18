4f7acf2e406cb55cf3c6d39e1326650ab1de3e6c
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-18 16:57:23 -0300

    Projeto Atualizado

--- Projeto/ProjetoApp.java
+++ Projeto/ProjetoApp.java
@@ -10,17 +10,24 @@ import figures.*;
 class ProjetoApp {
     public static void main (String[] args) {
         ProjetoFrame frame = new ProjetoFrame();
-        frame.setVisible(true);
+        frame.setVisible(true);	
     }
 }
 
-class ProjetoFrame extends JFrame {
-    ArrayList<Figure> figs = new ArrayList<Figure>();
+class ProjetoFrame extends JFrame{
+	
+    public ArrayList<Figure> figs = new ArrayList<Figure>();
     Iterator<Figure> obj = figs.iterator();
     public Figure focus = null;
     Random rand = new Random();
+ 
+    int cont = 0;
+    boolean Bview = false;	
+	
+    JPopupMenu mudarCor;
+    Point start;	
 
-    ProjetoFrame () {
+    public ProjetoFrame () {
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
@@ -28,21 +35,109 @@ class ProjetoFrame extends JFrame {
                 }
             }
         );
-
+   	
+	this.setTitle("Figuras");
+        this.setSize(600, 500);
+    
 	this.addMouseListener(new MouseAdapter(){
 		public void mousePressed (MouseEvent evt){
+			start = evt.getPoint();
 			focus = null;
-			for(Figure fig: figs){
-				if((fig.x <= evt.getX() && fig.x+fig.w >= evt.getX()) 
-				&& (fig.y <= evt.getY() && fig.y+fig.h >= evt.getY())){
-					focus = fig;
+			if(SwingUtilities.isLeftMouseButton(evt)){
+				for(Figure fig: figs){
+					if(fig.contains(evt)){
+						focus = fig;					
+					}
 				}
+				if(Bview){
+					mudarCor.setVisible(false);
+					Bview = false;
+					cont-=1;
+				}
+				if(focus != null){
+					figs.remove(focus);
+					figs.add(focus);
+				}
+				repaint();
+		
 			}
+			if(SwingUtilities.isRightMouseButton(evt)){
+				for(Figure fig: figs){
+					if(fig.contains(evt)){
+						focus = fig;
+					}
+				}
+				if(Bview){
+					mudarCor.setVisible(false);
+					Bview = false;
+					cont-=1;
+				}
+				if(focus != null){
+					mudarCor = new JPopupMenu();
+					JMenuItem fundoC = new JMenuItem();
+					fundoC.setText("Fundo");
+					fundoC.addActionListener(
+  						new ActionListener() {
+   							public void actionPerformed(ActionEvent evtChangeBgd) {
+								mudarCor.setVisible(false);
+								Bview = false;
+								cont -= 1;  
+      								JColorChooser color1 = new JColorChooser();
+								Color cor1 = JColorChooser.showDialog(null, "Escolha a Cor",focus.bgd);
+								if(cor1 != null){
+									focus.bgd = cor1;
+    								}
+								repaint();
+  					 		}	
+						}
+					);
+					JMenuItem contornoC = new JMenuItem();
+					contornoC.setText("Contorno");
+					contornoC.addActionListener(
+  						new ActionListener() {
+   							public void actionPerformed(ActionEvent evtChangeOl) {
+								mudarCor.setVisible(false);
+								Bview = false;
+								cont -= 1; 
+      								JColorChooser color2 = new JColorChooser();
+								Color cor2 = JColorChooser.showDialog(null, "Escolha a Cor",focus.ol);
+								if(cor2 != null){
+									focus.ol = cor2;
+    								}
+								repaint();							
+  					 		}
+  					 	}
+						
+					);
+					mudarCor.add(fundoC);
+					mudarCor.add(contornoC);
+					if(cont == 0){
+						mudarCor.show(null, evt.getX(), evt.getY());
+						cont += 1;
+						Bview = true;
+					}
+				}
+				repaint();	
+	       		}
+			if(focus== null && Bview){
+					mudarCor.setVisible(false);
+					Bview = false;
+					cont -= 1;
+			}	
 		}
 	});
 
 	this.addMouseMotionListener(new MouseMotionAdapter(){
 		public void mouseDragged (MouseEvent evt){
+			if(focus != null){
+				if(start != null){
+					Point Atual = evt.getPoint();
+					focus.drag((int)(Atual.getX() - start.x),(int)(Atual.getY() - start.y));
+					start = Atual;
+					repaint();
+				}
+			}
+			
 		
 		}
 	});
@@ -54,10 +149,10 @@ class ProjetoFrame extends JFrame {
                       int y = MouseInfo.getPointerInfo().getLocation().y;
                       int w = 50;
                       int h = 50;
-		      int l = rand.nextInt(100);
-                      int p = h + rand.nextInt(100);
-		      Color bgd = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
-                      Color ol = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+		      int l = 30;
+                      int p = 60;		
+		      Color bgd = new Color(255,255,255);
+                      Color ol = new Color(0,0,0);
 		
 		      if (evt.getKeyChar() == 'r') {
 			Rect r = new Rect(x,y,w,h,ol,bgd);
@@ -68,25 +163,51 @@ class ProjetoFrame extends JFrame {
 		      } else if(evt.getKeyChar() == 's') {
 			Seta s = new Seta(x,y,w,h,l,p,ol,bgd);
                       	figs.add(s);
-		      } else if(evt.getKeyChar() == 'l') {
-			Linha li = new Linha(x,y,w,h,ol);
-                      	figs.add(li);
-		      } else if(evt.getKeyCode() == java.awt.event.KeyEvent.VK_DELETE) {
+		      } else if(evt.getKeyChar() == 't') {
+			Triangle t = new Triangle(x,y,w,h,ol,bgd);
+                      	figs.add(t);
+		      } else if(evt.getKeyCode() == KeyEvent.VK_DELETE) {
 			for(Iterator<Figure> iterator = figs.iterator(); iterator.hasNext();){
 		        	Figure obj = iterator.next();
 				if(obj == focus){
 					iterator.remove();
 				}
 		        }
-			
 		      }
+		      else if(evt.getKeyChar() == '+') {
+			if(focus != null){
+				focus.redimension(5);
+			}
+		      }
+		      else if(evt.getKeyChar() == '-') {
+			if(focus != null && (focus.w >= 6 && focus.h >= 6)){
+				focus.redimension(-5);
+			}
+		      }
+		      else if(evt.getKeyCode() == KeyEvent.VK_UP) {
+			if(focus != null && focus.y >= 1){
+				focus.drag(0,-5);
+			}
+		      }
+		      else if(evt.getKeyCode() == KeyEvent.VK_DOWN) {
+			if(focus != null &&  focus.y<= getHeight()-1){
+				focus.drag(0, 5);
+			}
+		      }  
+		      else if(evt.getKeyCode() == KeyEvent.VK_LEFT) {
+			if(focus != null && focus.x >= 1){
+				focus.drag(-5,0);
+			}
+		      }
+		      else if(evt.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if(focus != null && focus.x<= getWidth()-1){
+				focus.drag(5,0);
+			}
+		      }   
                       repaint();
 	      }
 	   }
 	);
-	
-        this.setTitle("Figuras");
-        this.setSize(400, 400);
     }
 
     public void paint (Graphics g) {
