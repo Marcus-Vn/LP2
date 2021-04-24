8f8b6fe9d1842f9f80ebfcf592428c77db8b7547
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-04-24 18:04:24 -0300

    Triangulo com contorno de foco

--- Projeto/figures/Triangle.java
+++ Projeto/figures/Triangle.java
@@ -8,6 +8,7 @@ import java.awt.event.*;
 public class Triangle extends Figure {
 
     Polygon Triangle;
+    Polygon foco;
 	
     public Triangle(int x, int y, int w, int h ,Color ol,Color bgd) {
         this.x = x;
@@ -42,17 +43,20 @@ public class Triangle extends Figure {
     public void drag (int dx, int dy) {
 	this.x += dx;
 	this.y += dy;
-	int[] xValues = { this.x, this.x+(this.w/2), this.x+this.w};
-        int[] yValues = { this.y+this.h, this.y, this.y + this.h,};
-	this.Triangle = new Polygon( xValues, yValues, 3);
     }
 	
     public void redimension (int d) {
 	this.w += d;
 	this.h += d;
-	int[] xValues = { this.x, this.x+(this.w/2), this.x+this.w};
-        int[] yValues = { this.y+this.h, this.y, this.y + this.h,};
-	this.Triangle = new Polygon( xValues, yValues, 3);
+    }
+
+    public void InFocus(Graphics g){
+	Graphics2D focusline = (Graphics2D) g;
+	int[] xFocus = { this.x-5, this.x+(this.w/2), this.x+this.w+5};
+        int[] yFocus = { this.y+this.h+3, this.y-6, this.y + this.h+3};	
+	this.foco = new Polygon( xFocus, yFocus, 3);
+	focusline.setColor(Color.red); 
+	focusline.draw(this.foco);	
     }
     
 }
