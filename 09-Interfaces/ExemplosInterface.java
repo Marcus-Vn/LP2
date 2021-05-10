826b80c24c33d48e18c7a68303396f57cf5c4a1f
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-05-10 11:21:59 -0300

    Atualização exemplos

--- 09-Interfaces/ExemplosInterface.java
+++ 09-Interfaces/ExemplosInterface.java
@@ -1,8 +1,9 @@
-//Pensando em jogos:
+Pensando em jogos:
 
-//"Coletáveis"
-//Itens para coletar e usar depois
-
+"Coletáveis"
+Itens para coletar e usar depois.
+O parâmetro number significa o número de itens coletados e utilizados
+	
 interface Collectable {
 	void Collect (int number);
 	void Use (int number);
@@ -35,37 +36,40 @@ class CrystalPoints implements Collectable {
 	}
 }
 
-//"Armas"
-//Cada uma tem um ataque rápido (mais fraco) e um forte (mais lento).
+"Armas"
+O parâmetro damage se refere ao dano e time ao tempo de execução.
+Cada arma teria ataques com animações diferentes, ciclos de ataque diferentes, etc. Mas os métodos seriam similares.
+Cada uma tem um ataque rápido (mais fraco) e um forte (mais lento).
+
 
 interface Attacker {
-	void FastAttack (void);
-	void StrongAttack (void);
+	void FastAttack (int damage, int time);
+	void StrongAttack (int damage, int time);
 }
 
 class Sword implements Attacker {
-	void FastAttack (void) {
+	void FastAttack (int damage, int time) {
 		//Ataque rápido com espada
 	}
-	void StrongAttack (void) {
+	void StrongAttack (int damage, int time) {
 		//Ataque forte com espada, demora mais
 	}
 }
 
 class Bow implements Attacker {
-	void FastAttack (void) {
+	void FastAttack (int damage, int time) {
 		//Tiro rápido de flecha
 	}
-	void StrongAttack (void) {
+	void StrongAttack (int damage, int time) {
 		//Tiro carregado de flecha
 	}
 }
 
 class MagicStaff implements Attacker {
-	void FastAttack (void){
+	void FastAttack (int damage, int time){
 		//Ataque mágico rápido com cajado 
 	}
-	void StrongAttack (void) {
+	void StrongAttack (int damage, int time) {
 		//Ataque mágico carregado com cajado 
 	}
 }
