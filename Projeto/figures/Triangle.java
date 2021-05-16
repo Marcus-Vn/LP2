38f527ebdc3e614c5cc049dd746ea41da9ea9d58
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-05-16 19:04:22 -0300

    Atualização Triângulo

--- Projeto/figures/Triangle.java
+++ Projeto/figures/Triangle.java
@@ -10,52 +10,34 @@ public class Triangle extends Figure {
     Polygon foco;
 	
     public Triangle(int x, int y, int w, int h ,Color ol,Color bgd) {
-        this.x = x;
-        this.y = y;
-	this.w = w;
-        this.h = h;
-	this.ol = ol;
-	this.bgd = bgd;
+    	super(x,y,w,h,ol,bgd);
     }
 
-    public void paint (Graphics g) {
-	Graphics2D g2d = (Graphics2D) g;
-	int[] xValues = { this.x, this.x+(this.w/2), this.x+this.w};
+    public void paint (Graphics g, boolean focused) {
+    	Graphics2D g2d = (Graphics2D) g;
+    	int[] xValues = { this.x, this.x+(this.w/2), this.x+this.w};
         int[] yValues = { this.y+this.h, this.y, this.y + this.h,};
-	
-	this.Triangle = new Polygon( xValues, yValues, 3);
+        this.Triangle = new Polygon( xValues, yValues, 3);
         g2d.setStroke(new BasicStroke(2));
-	g2d.setColor(this.bgd);
-	g2d.fill(this.Triangle); 
-	g2d.setColor(this.ol); 
-	g2d.draw(this.Triangle); 
+        g2d.setColor(this.bgd);
+        g2d.fill(this.Triangle); 
+        g2d.setColor(this.ol); 
+        g2d.draw(this.Triangle);
+        if (focused) {
+        	Graphics2D focusline = (Graphics2D) g;
+        	int[] xFocus = { this.x-5, this.x+(this.w/2), this.x+this.w+5};
+        	int[] yFocus = { this.y+this.h+3, this.y-6, this.y + this.h+3};	
+        	this.foco = new Polygon( xFocus, yFocus, 3);
+        	focusline.setColor(Color.red); 
+        	focusline.draw(this.foco);	
+        }
         
     }
 	
-    public boolean contains(MouseEvent evt) {
-        if (this.Triangle.contains(evt.getPoint())){
+    public boolean clicked(int x, int y) {
+        if (this.Triangle.contains(x,y)){
             	return true;
 	}
 	else{ return false;}
     }
-
-    public void drag (int dx, int dy) {
-	this.x += dx;
-	this.y += dy;
-    }
-	
-    public void redimension (int d) {
-	this.w += d;
-	this.h += d;
-    }
-
-    public void InFocus(Graphics g){
-	Graphics2D focusline = (Graphics2D) g;
-	int[] xFocus = { this.x-5, this.x+(this.w/2), this.x+this.w+5};
-        int[] yFocus = { this.y+this.h+3, this.y-6, this.y + this.h+3};	
-	this.foco = new Polygon( xFocus, yFocus, 3);
-	focusline.setColor(Color.red); 
-	focusline.draw(this.foco);	
-    }
-    
 }
