f57894ddceeb3b50ceff92e25deb5c3ecd7ebb57
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2021-05-16 19:02:48 -0300

    Atualização Seta

--- Projeto/figures/Seta.java
+++ Projeto/figures/Seta.java
@@ -7,72 +7,59 @@ public class Seta extends Figure {
 
     Polygon Seta;
     Polygon foco;	
-
-    //extensão da ponta
+    //extensao da ponta
     int l, p;
 
     public Seta (int x, int y, int w, int h, int l, int p, Color ol, Color bgd) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-	this.l = l;
+    	super(x,y,w,h,ol,bgd);
+        this.l = l;
         this.p = p;
-	this.ol = ol;
-	this.bgd = bgd;
     }
 
-    public void paint (Graphics g) {
-	Graphics2D g2d = (Graphics2D) g;
-	int[] xValues = { this.x, this.x+this.w, this.x+this.w, this.x+this.w+this.p, 
-	this.x + this.w, this.x + this.w, this.x};
+    public void paint (Graphics g, boolean focused) {
+    	Graphics2D g2d = (Graphics2D) g;
+    	int[] xValues = { this.x, this.x+this.w, this.x+this.w, this.x+this.w+this.p, 
+    	this.x + this.w, this.x + this.w, this.x};
         int[] yValues = { this.y, this.y, this.y - this.l, this.y + this.h/2,
-	this.l+this.h+this.y, this.y + this.h, this.y + this.h};
+        this.l+this.h+this.y, this.y + this.h, this.y + this.h};
 	
-	this.Seta = new Polygon( xValues, yValues, 7 );
+        this.Seta = new Polygon( xValues, yValues, 7 );
         g2d.setStroke(new BasicStroke(2));
-	g2d.setColor(this.bgd);
-	g2d.fill(this.Seta); 
-	g2d.setColor(this.ol); 
-	g2d.draw(this.Seta); 
+        g2d.setColor(this.bgd);
+        g2d.fill(this.Seta); 
+        g2d.setColor(this.ol); 
+        g2d.draw(this.Seta);
+        if (focused) {
+        	Graphics2D focusline = (Graphics2D) g;
+        	int[] xFocus = { this.x-3, this.x+this.w-3, this.x+this.w-3,this.x+this.w, this.x+this.w+this.p+6, 
+        	this.x + this.w,this.x + this.w-3, this.x + this.w-3, this.x-3};
+            	int[] yFocus = { this.y-3, this.y-3, this.y - this.l-6,this.y - this.l-6, this.y + this.h/2,
+            	this.l+this.h+this.y+6,this.l+this.h+this.y+6, this.y + this.h+3, this.y + this.h+3};
+            	this.foco = new Polygon( xFocus, yFocus, 9 );
+            	focusline.setColor(Color.red); 
+            	focusline.draw(this.foco);	
+        }
         
     }
 	
-    public boolean contains(MouseEvent evt) {
-        if (this.Seta.contains(evt.getPoint())){
+    public boolean clicked(int x, int y) {
+        if (this.Seta.contains(x,y)){
             	return true;
 	}
 	else{ return false;}
     }
 
-    public void drag (int dx, int dy) {
-	this.x += dx;
-	this.y += dy;	
-    }
-
     public void redimension(int d) {
-	this.w += d;
-	this.h += d;
-	if(d < 0){
-		if(this.p >= 6 && this.l >=6){
-			this.p += d;
-			this.l += d;
+    	this.w += d;
+    	this.h += d;
+    	if(d < 0){
+    		if(this.p >= 6 && this.l >=6){
+    			this.p += d;
+    			this.l += d;
 		}
-	}else{
-		this.p += d;
-		this.l += d;
-	}
-    }
-	
-    public void InFocus(Graphics g){
-	Graphics2D focusline = (Graphics2D) g;
-	int[] xValues = { this.x-3, this.x+this.w-3, this.x+this.w-3,this.x+this.w, this.x+this.w+this.p+6, 
-	this.x + this.w,this.x + this.w-3, this.x + this.w-3, this.x-3};
-        int[] yValues = { this.y-3, this.y-3, this.y - this.l-6,this.y - this.l-6, this.y + this.h/2,
-	this.l+this.h+this.y+6,this.l+this.h+this.y+6, this.y + this.h+3, this.y + this.h+3};
-	this.foco = new Polygon( xValues, yValues, 9 );
-	focusline.setColor(Color.red); 
-	focusline.draw(this.foco);	
-    }
-    
+    	}else{
+    		this.p += d;
+    		this.l += d;
+    	}    	
+    }   
 }
